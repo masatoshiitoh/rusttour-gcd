2e8738d25daf95696169f90c7fadcf349bb95982
Masatoshi Itoh <masatoshi9953@gmail.com>
2024-08-11 15:01:31 +0900

    add some tests

--- src/main.rs
+++ src/main.rs
@@ -37,9 +37,20 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
 
 #[test]
 fn test_gcd() {
-    println!("Running Unit tests!");
+    println!("正常系テスト");
     assert_eq!(gcd(14, 15), 1);
     assert_eq!(
         gcd(2*3*5*11*17,
             3*7*11*13*19), 3*11);
+}
+
+#[test]
+#[should_panic]
+fn test_gcd_arg1_zero() {
+    gcd(0, 1);
+}
+#[test]
+#[should_panic]
+fn test_gcd_arg2_zero() {
+    gcd(1, 0);
 }
